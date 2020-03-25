d1eb12116bbca192d12c333a90d05ce7d0565acd
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-25 18:18:30 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,6 +5,14 @@ class CashRegister
   def initialize(discount = 20)
     @total = 0
     @discount = discount
+    @item = []
   end
+
+  def add_item
+  end
+
+  def apply_discount
+  end
+
 
   end
