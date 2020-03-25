9e119e0816ee52ee7b411a85fbbee837a6f17ac0
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-25 18:17:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,11 +2,9 @@ class CashRegister
 
   attr_accessor :total, :discount
 
-  def initialize(total = 0)
-    @total = total
+  def initialize(discount = 200)
+    @total = 0
+    @discount = discount
   end
 
-  def discount
-    self.discount
-  end
   end
