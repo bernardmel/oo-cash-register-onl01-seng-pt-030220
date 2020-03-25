d599707e2d7cbaad86bb55c62287977c2e7a7a14
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-25 18:50:48 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,9 +17,18 @@ class CashRegister
     end
 end
 
-
-  def apply_discount
+def apply_discount
+    if @discount > 0
+      @take_off = (price * discount)/100
+      @total -= @take_off
+      return "After the discount, the total comes to $#{total}."
+    else
+      return "There is no discount to apply."
+    end
   end
 
+  def void_last_transaction
+    @total -= @price
+  end
 
   end
