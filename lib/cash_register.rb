71a45fdbfc02132ff1cdef0743bef4dd7a590473
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-25 18:37:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   def add_item(item, price, quantity = 1)
     @price = price
     @total += price * quantity
-    if quantitiy > 1
+    if quantity > 1
       counter = 0
       while counter < quantity
         @items << items
