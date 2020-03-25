16ef24b5b75af00f2a8ca4b3d94009c0d3f7d8a6
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-25 18:37:46 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
     if quantity > 1
       counter = 0
       while counter < quantity
-        @items << items
+        @items << item
         counter += 1
       end
     else
